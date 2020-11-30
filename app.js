9d49b4ea039fb045ef4e2b4d9832a80d10ede014
cheeyee627 <74081886+cheeyee627@users.noreply.github.com>
2020-11-30 17:55:51 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ let api={
     units:'metric'
 }
 let searchmethod='q'
-
+adjustMain();
 
 const weather = {}
 const searchbox=document.getElementById('userInputtxt');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+}
 
 //searchbox.addEventListener('keypress',setQuery);
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
+
 
 
 weather.temperature = {
@@ -51,8 +59,8 @@ function getLocation(latitude, longitude){
             weather.country = data.sys.country;
             let searchbox=document.querySelector('.search-box');
             searchbox.value= `${weather.city}, ${weather.country}`;
-        })
-        
+            getResults(searchbox.value);
+        }) 
 }
 
 // SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
@@ -61,15 +69,13 @@ function showError(error){
     notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
-function execute(){
-    if(searchbox.value)
-        getResults(searchbox.value);
-}
+
 
 function getResults(query){
     fetch(`${api.base}weather?${searchmethod}=${query}&units=${api.units}&APPID=${api.key}`)
     .then(function(response){
         let data = response.json();
+        console.log(data)
         return data;
     })
     .then(data => {
@@ -80,11 +86,15 @@ function getResults(query){
     })
     .then(function(){
         displayResults();
-    });
+    })
 }
+    
 
 function extractResults(jsonResult){
     console.log(jsonResult)
+    if (jsonResult.hasOwnProperty('message')){
+        alert("Cannot find location! Retry by typing in the format of 'city','country code'.")
+    }
     weather.temperature.value = Math.round(jsonResult.main.temp);
     weather.condition = jsonResult.weather[0].description;
     weather.iconId = jsonResult.weather[0].icon;
@@ -92,6 +102,7 @@ function extractResults(jsonResult){
     weather.country = jsonResult.sys.country;
     weather.background = jsonResult.weather[0].main
     weather.currentTime=jsonResult.dt
+    weather.currentTimeZone=jsonResult.timezone
 }
 
 function switchBackground (){
@@ -117,19 +128,19 @@ function switchBackground (){
             break;
             
     }
+
 }
 function adjustMain() {
-    let main_border=document.getElementById('mainBox');
+    let main_border=document.getElementById('TempDescrip');
     let main_border_height=main_border.clientHeight;
     let main_border_width=main_border.clientWidth;
-
-    main_border.style.left= `calc(50%-${main_border_width/2}px)`;
-    main_border.style.top= `calc(50%-${main_border_height/1.3}px)`;
-    main_border.style.visibility= 'visible';
+ 
+    main_border.style.left= "calc(50% - " + (main_border_width/2).toString() + "px)";  
+    main_border.style.top= "calc(50% - " + (main_border_height/1.3).toString() + "px)"
 }
    
 const iconElement = document.querySelector(".weather-icon");
-const city = document.getElementById('city');
+//const city = document.getElementById('city');
 const date=document.getElementById('date');
 const time=document.getElementById('time');
 const temp=document.getElementById('temp');
@@ -139,45 +150,50 @@ function displayResults (){
     console.log(weather)
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
     temp.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    weather_el.innerHTML = weather.condition;
-    city.innerHTML = `${weather.city}, ${weather.country}`;
-    let now=new Date()
-    date.innerHTML = dateBuilder(now)
-    time.innerHTML = 'Current Time: '+ timeBuilder(now)
-    adjustMain();
+    weather_el.innerHTML = weather.condition[0].toUpperCase() +  weather.condition.slice(1);
+    //weather_el.innerHTML = weather.condition;
+    //city.innerHTML = `${weather.city}, ${weather.country}`;
+    time.innerHTML = timeBuilder()
+    searchbox.value=`${weather.city}, ${weather.country}`;
 }
 
-function dateBuilder (d){
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    let day = days[d.getDay()];
-    let date= d.getDate()
-    let month = months[d.getMonth()];
-    let year=d.getFullYear();
+function timeBuilder (){
+    
+    var initial= new Date((weather.currentTime*1000));
+    var new_initial=initial.setHours(initial.getHours() - 8)
 
-    return `${day} ${date} ${month} ${year}` ;
-    }
+    const milliseconds2=weather.currentTimeZone*1000;
+    const totalseconds=new_initial+milliseconds2;
 
-function timeBuilder (now){
-    // Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-var time_current = new Date(weather.currentTime*1000);
-console.log(time_current)
+    var time_current = new Date(totalseconds);
+    
 // Hours part from the timestamp
 var hours = time_current.getHours();
+var temp = hours.toString();
+    if (temp.length<2){
+        hours="0"+ hours.toString()
+    }  
 // Minutes part from the timestamp
-var minutes = "0" + time_current.getMinutes();
-// Seconds part from the timestamp
-var seconds = "0" + time_current.getSeconds();
+var minutes =time_current.getMinutes();
+var temp = minutes.toString();
+    if (temp.length<2){
+        minutes="0"+ minutes.toString()
+    }
 
+var seconds =time_current.getSeconds();
+var temp = seconds.toString();
+    if (temp.length<2){
+        seconds="0"+ seconds.toString()
+    }
 // Will display time in 10:30:23 format
-return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    mins_input=now.getMinutes
-    if (mins_input.length<2)
-        mins_input="0"+ now.getMinutes
-        else mins_input=now.getminutes
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    return now.getHours() + ":" + now.getMinutes() ;
-    
+    let day = days[time_current.getDay()];
+    let date= time_current.getDate()
+    let month = months[time_current.getMonth()];
+    let year=time_current.getFullYear();
+return `${day}, ${date} ${month} ${year}` +', '  + hours + ':' + minutes + ':'+ seconds
+
 }
